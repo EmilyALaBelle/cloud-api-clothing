66bccae7584da2736ec4caad359e27f001ae450d
EmilyALaBelle <emilylabelle8@gmail.com>
2022-10-21 15:10:31 -0400

    commit

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 import functions from "firebase-functions"
 import express from "express"
-import {createClothing, getAllClothing, updateClothing} from './src/clothing.js'
+import {createClothing, getAllClothing, updateClothing, deleteClothing, getOneClothing} from './src/clothing.js'
 
 const app = express()
 app.use(express.json())
@@ -8,6 +8,8 @@ app.use(express.json())
 app.post('/clothing', createClothing)
 app.get('/clothing', getAllClothing)
 app.patch('/clothing/:uid', updateClothing)
+app.delete('/clothing/:uid', deleteClothing)
+app.get('/clothing/:uid', getOneClothing)
 
 app.get('/test', (req, res) => res.send('Our first cloud api works!'))
 
--- functions/src/clothing.js
+++ functions/src/clothing.js
@@ -8,25 +8,39 @@ export async function createClothing(req, res) {
 }
 
 export async function getAllClothing(req, res) {
-const db = dbConnect()
-const collection = await db.collection('clothing').get()
-.catch(err => res.status(500).send({success: false, message: err }))
-const clothing = collection.docs.map(doc => {
-    let user = doc.data()
-    user.uid = doc.id
-    return user
-})
-res.send(clothing)
+    const db = dbConnect()
+    const collection = await db.collection('clothing').get()
+        .catch(err => res.status(500).send({ success: false, message: err }))
+    const clothing = collection.docs.map(doc => {
+        let clothing = doc.data()
+        clothing.uid = doc.id
+        return clothing
+    })
+    res.send(clothing)
 }
 
 export async function updateClothing(req, res) {
     const { uid } = req.params
     const db = dbConnect()
     const doc = await db.collection('clothing').doc(uid).update(req.body)
-    .catch(err => res.status(500).send({success: false, message: err}))
-    res.status(202).send({success: true, message: 'Clothing Updated ' + doc.id})
+        .catch(err => res.status(500).send({ success: false, message: err }))
+    res.status(202).send({ success: true, message: 'Clothing Updated ' + doc.id })
 }
 
-export async function deleteClothing(req, res){
-    
+export async function deleteClothing(req, res) {
+    const { uid } = req.params
+    const db = dbConnect()
+    const doc = await db.collection('clothing').doc(uid).delete()
+        .catch(err => res.status(500).send({ success: false, message: err }))
+    res.status(202).send({ success: true, message: 'Clothing Deleted' })
 }
+
+export async function getOneClothing(req, res) {
+    const { uid } = req.params
+    const db = dbConnect()
+    const doc = await db.collection('clothing').doc(uid).get()
+        .catch(err => res.status(500).send({ success: false, message: err }))
+        res.status(202).send({ success: true, message: doc.data()})
+}
+    
+

a8a1cefb10a5dbc81de6174c6554553b54fe6b03
EmilyALaBelle <emilylabelle8@gmail.com>
2022-10-21 12:35:53 -0400

    commit

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,11 @@
-const functions = require("firebase-functions");
+import functions from "firebase-functions"
+import express from "express"
+//import {} from './src/clothing.js'
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express()
+app.use(express.json())
+
+
+export const api = functions.https.onRequest(app)
+
+
